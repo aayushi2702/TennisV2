d1cbc7ddcad64ce06518875d9f295c8d4873f289
Ayushi <aayushijain2702@gmail.com>
2022-12-12 15:51:46 +0530

    Refactor : Removing boiler plate code to increase readability.

--- TennisV2/src/main/java/com/tennis/game/service/TennisGameService.java
+++ TennisV2/src/main/java/com/tennis/game/service/TennisGameService.java
@@ -1,5 +1,7 @@
 package com.tennis.game.service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import org.springframework.stereotype.Service;
@@ -7,43 +9,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class TennisGameService {
 
-	private static final String SCORE_LOVE_ALL = "Love All";
-	private static final String SCORE_FIFTEEN_ALL = "Fifteen All";
-	private static final int ZERO = 0;
-	private static final int ONE = 1;
-	private static final int TWO = 2;
-	private static final String SCORE_THIRTY_ALL = "Thirty All";
 	private static final String DEUCE = "Deuce";
 	private static final String ADVANTAGE = "Advantage";
 	private static final String PLAYERONE = "PlayerOneName";
 	private static final String PLAYERTWO = "PlayerTwoName";
 	private static final String GAME = "Game";
-	private static final String SCORE_LOVE_FIFTEEN = "Love Fifteen";
 	private static final String SCORE_LOVE = "Love";
 	private static final String SCORE_FIFTEEN = "Fifteen";
 	private static final String SCORE_THIRTY = "Thirty";
 	private static final String SCORE_FORTY = "Forty";
+	private static final String STRING_ALL = "All";
+	private static final Map<Integer, String> SCORE_MAP = new HashMap<>();
+
+	static {
+		SCORE_MAP.put(0, SCORE_LOVE);
+		SCORE_MAP.put(1, SCORE_FIFTEEN);
+		SCORE_MAP.put(2, SCORE_THIRTY);
+		SCORE_MAP.put(3, SCORE_FORTY);
+	}
 
 	public String getGameScore(Integer playerOneScore, Integer playerTwoScore) {
 		if (Objects.equals(playerOneScore, playerTwoScore)) {
-			if (playerOneScore == ZERO) {
-				return SCORE_LOVE_ALL;
-			} else if (playerOneScore == ONE) {
-				return SCORE_FIFTEEN_ALL;
-			} else if (playerOneScore == TWO) {
-				return SCORE_THIRTY_ALL;
-			} else {
-				return DEUCE;
-			}
+			return playerOneScore > 2 ? DEUCE : getPlayerScore(playerOneScore).concat(" ").concat(STRING_ALL);
 		} else {
 			if (Math.max(playerTwoScore, playerOneScore) > 3) {
-				if (pointDifferenceIsOne(playerOneScore, playerTwoScore)) {
-					return ADVANTAGE.concat(" ").concat(getHighScorerPlayerName(playerOneScore, playerTwoScore));
-				} else {
-					return GAME.concat(" ").concat(getHighScorerPlayerName(playerOneScore, playerTwoScore));
-				}
-			}else {
-				return getPlayerScore(playerOneScore) +" "+ getPlayerScore(playerTwoScore);
+				return (pointDifferenceIsOne(playerOneScore, playerTwoScore) ? ADVANTAGE : GAME).concat(" ")
+						.concat(getHighScorerPlayerName(playerOneScore, playerTwoScore));
+			} else {
+				return getPlayerScore(playerOneScore).concat(" ").concat(getPlayerScore(playerTwoScore));
 			}
 		}
 	}
@@ -55,16 +48,9 @@ public class TennisGameService {
 	private String getHighScorerPlayerName(int playerOneScore, int playerTwoScore) {
 		return playerOneScore > playerTwoScore ? PLAYERONE : PLAYERTWO;
 	}
-	
+
 	private String getPlayerScore(int playerScore) {
-		if(playerScore==0)
-			return SCORE_LOVE;
-		else if(playerScore==1)
-			return SCORE_FIFTEEN;
-		else if(playerScore==2)
-			return SCORE_THIRTY;
-		else
-			return SCORE_FORTY;
+		return SCORE_MAP.get(playerScore);
 	}
 
 }
